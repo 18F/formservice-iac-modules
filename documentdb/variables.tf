421e501e880938476c3aa6a5bba9b220f83217f1
Leonard Becraft <leonard.becraft@gsa.gov>
2021-01-23 10:17:43 -0500

    added variables

--- documentdb/variables.tf
+++ documentdb/variables.tf
@@ -8,29 +8,29 @@ variable "name_prefix" {
   default = "test-name"
 }
                                 
-  variable "cluster_size"             {}             
-  variable "master_username"          {}         
-  variable "master_password"          {}     
-  variable "instance_class"           {}  
-  variable "vpc_id"                   {} 
+variable "cluster_size"             {}             
+variable "master_username"          {}         
+variable "master_password"          {}     
+variable "instance_class"           {}  
+variable "vpc_id"                   {} 
 
-  variable "subnet_ids" {
-    type        = list(string)
-    default     = []
-    description = "List of subnet IDs to install DocumentDB cluster into"
-  }
+variable "subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "List of subnet IDs to install DocumentDB cluster into"
+}
 
-  variable "allowed_security_groups" {
-    type        = list(string)
-    default     = []
-    description = "List of existing Security Groups to be allowed to connect to the DocumentDB cluster"
-  }
+variable "allowed_security_groups" {
+  type        = list(string)
+  default     = []
+  description = "List of existing Security Groups to be allowed to connect to the DocumentDB cluster"
+}
 
-  variable "allowed_cidr_blocks" {
+variable "allowed_cidr_blocks" {
   type        = list(string)
   default     = []
   description = "List of CIDR blocks to be allowed to connect to the DocumentDB cluster"
 }
-  variable "zone_id" {
-    default = ""
-  } 
+variable "zone_id" {
+  default = ""
+} 
