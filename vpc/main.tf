881c46079469e30459b58b9c4edcbde816f2593c
MichaelJ <michael.p.jones@gsa.gov>
2021-06-16 11:05:49 -0400

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -131,7 +131,7 @@ resource "aws_ec2_transit_gateway" "tgw" {
 # }
 
 module "vpc_endpoints" {
-  source = "../../modules/vpc-endpoints"
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
 
   vpc_id             = module.vpc.vpc_id
   security_group_ids = [data.aws_security_group.default.id]
@@ -209,7 +209,8 @@ module "vpc_endpoints" {
       service             = "codedeploy-commands-secure"
       private_dns_enabled = true
       subnet_ids          = module.vpc.private_subnets
-    }
+    },
+      
   }
 }
 ################################################################################
