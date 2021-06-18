277ceac0d83d5497d3f1228129d34ee6c3388cea
MichaelJ <michael.p.jones@gsa.gov>
2021-06-17 23:53:52 -0400

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -14,16 +14,3 @@ output "linux_bastion_public_ip" {
 output "linux_bastion_sg_id" {
   value = aws_security_group.linux_bastion.id
 }
-
-output "windows_bastion_arn" {
-  value = module.ec2_windows.arn
-}
-output "windows_bastion_private_ip" {
-  value = module.ec2_windows.private_ip
-}
-output "windows_bastion_public_ip" {
-  value = module.ec2_windows.public_ip
-}
-output "windows_bastion_sg_id" {
-  value = aws_security_group.win_bastion.id
-}
