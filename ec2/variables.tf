0712cc47d1c9ea1d1e5aa8d1062e28816fd005f5
MichaelJ <michael.p.jones@gsa.gov>
2021-06-17 23:55:56 -0400

    adjustments for terraform linux instance

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -5,17 +5,14 @@
 variable "name_prefix" {
   type        = string
   description = "Used for overall naming of resources"
-  default     = "ide-sandb-hyperscience"
+  default     = "terraform linux instance"
 }
 
 variable "linux_ami" {}
-variable "windows_ami" {}
 
 variable "linux_instance_type" {}
-variable "windows_instance_type" {}
 
 variable "linux_monitoring" {default = "true"}
-variable "windows_monitoring" {default = "true"}
 
 variable "vpc_id" {}
 variable "subnet_id" {}
@@ -28,25 +25,7 @@ variable "linux_root_block_size" {
   default     = "50"
 }
 
-variable "windows_root_block_size" {
-  type        = string
-  description = "Size in GB of root_block_device"
-  default     = "50"
-}
-
 variable "linux_ingress_cidr_blocks" {
-  type        = list(string)
-  default     = []
-  description = "List of sg ingress cidr blocks"
-}
-
-variable "windows_rdp_ingress_cidr_blocks" {
-  type        = list(string)
-  default     = []
-  description = "List of sg ingress cidr blocks"
-}
-
-variable "windows_tls_ingress_cidr_blocks" {
   type        = list(string)
   default     = []
   description = "List of sg ingress cidr blocks"
