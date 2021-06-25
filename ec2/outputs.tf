ee04cb10f1348949da0896ab20dac68973e21778
Michael Willetts <michael.willetts@gsa.gov>
2021-06-25 12:20:10 -0500

    Clear Bug In MGMT EC2 Module

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -2,15 +2,15 @@
 # module/mgmt-hosts outputs
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-output "linux_bastion_arn" {
+output "linux_mgmt_arn" {
   value = module.ec2_linux.arn
 }
-output "linux_bastion_private_ip" {
+output "linux_mgmt_private_ip" {
   value = module.ec2_linux.private_ip
 }
-output "linux_bastion_public_ip" {
+output "linux_mgmt_public_ip" {
   value = module.ec2_linux.public_ip
 }
-output "linux_bastion_sg_id" {
-  value = aws_security_group.linux_bastion.id
+output "linux_mgmt_sg_id" {
+  value = aws_security_group.terraform_linux_sg.id
 }
