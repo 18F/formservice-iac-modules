3f4cad596d7edb47a56bfd9bdd6421e1b7126dcb
Michael Willetts <michael.willetts@gsa.gov>
2021-07-20 13:17:58 -0500

    prepare transit gateway

--- transit-gateway/main.tf
+++ transit-gateway/main.tf
@@ -13,28 +13,61 @@ terraform {
 }
 
 ##############################
+# Get Ogranizations OU
+##############################
+
+data "aws_organizations_organization" "org" {}
+
+data "aws_organizations_organizational_units" "ou" {
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
+##############################
 # transit gateway
 ##############################
 
 resource "aws_ec2_transit_gateway" "tgw" {
   auto_accept_shared_attachments = "enable"
-  tags = { Name = "${var.name_prefix}-TGW"}
+  tags = { Name = "${var.name_prefix}-tgw"}
+}
+
+##############################
+# RAM share for transit gateway
+##############################
+
+resource "aws_ram_resource_share" "tgw-share" {
+  name = "${var.name_prefix}-tgw"
+  allow_external_principals = false
+
+  tags = {
+    Environment = "FAAS"
+  }
+}
+
+resource "aws_ram_principal_association" "tgw-ram-principal-association" {
+  principal          = data.aws_organizations_organizational_units.ou.children[0].arn
+  resource_share_arn = aws_ram_resource_share.tgw-share.arn
+}
+
+resource "aws_ram_resource_association" "tgw-ram-resource-association" {
+  resource_arn       = aws_ec2_transit_gateway.tgw.arn
+  resource_share_arn = aws_ram_resource_share.tgw-share.arn
 }
 
 ## Attachment
-resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
+/* resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
    subnet_ids         = toset(module.vpc.public_subnets)
    transit_gateway_id = aws_ec2_transit_gateway.tgw.id
    vpc_id             = module.vpc.vpc_id
    tags = { Name = "${var.name_prefix}-TGW-attachment"}
-}
+} */
 
 ## Routes
-resource "aws_route" "tgw-route-one" {
+/* resource "aws_route" "tgw-route-one" {
    for_each = toset(concat(module.vpc.private_route_table_ids, module.vpc.public_route_table_ids))
   
    route_table_id         = each.value
    destination_cidr_block = "10.0.0.0/8"
    transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
    depends_on = [module.vpc, aws_ec2_transit_gateway_vpc_attachment.this]
-}
+} */
---
+++ transit-gateway/outputs.tf
@@ -0,0 +1,7 @@
+output "transit_gateway_arn" {
+  value = aws_ec2_transit_gateway.tgw.arn
+}
+
+output "transit_gateway_route_table_id" {
+  value = aws_ec2_transit_gateway.tgw.association_default_route_table_id
+}
