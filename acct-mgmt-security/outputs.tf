5f2cc69759f5bbdfb24fbd3d4a2d9289c5aaa401
Michael Willetts <michael.willetts@gsa.gov>
2021-08-11 15:45:47 -0500

    modify acct security outputs

--- acct-mgmt-security/outputs.tf
+++ acct-mgmt-security/outputs.tf
@@ -2,24 +2,16 @@
 # module/mgmt-security outputs
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-output "prod_ec2_key_name" {
+output "ec2_key_name" {
   value = aws_key_pair.prodkey.key_name
 }
 
-output "prod_ec2_key_arn" {
+output "ec2_key_arn" {
   value = aws_key_pair.prodkey.arn
 }
 
 output "beanstalk_ec2_role_arn" {
   value = aws_iam_instance_profile.aws-elasticbeanstalk-ec2-role.arn
-}
-
-output "code_s3_bucket_key_arn" {
-  value = aws_kms_key.s3_bucket_key.arn
-}
-
-output "code_s3_bucket_key_id" {
-  value = aws_kms_key.s3_bucket_key.key_id
 }
 
 /* output "documentdb_sg_id" {
