0d6d624a861785d86252853b85c4e4617934b859
Michael Willetts <michael.willetts@gsa.gov>
2021-08-14 18:31:31 -0500

    remove deadlock in vpc module

--- mgmt-vpc/main.tf
+++ mgmt-vpc/main.tf
@@ -101,7 +101,7 @@ module "vpc_endpoints" {
   source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
 
   vpc_id             = module.vpc.vpc_id
-  security_group_ids = var.endpointSGList
+  security_group_ids = [data.aws_security_group.default.id]
 
   endpoints = {
     s3 = {
