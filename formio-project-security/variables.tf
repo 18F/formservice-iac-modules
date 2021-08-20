b6ee30037f0d0fdd73f1682ae1d13c3d78d24661
Michael Willetts <michael.willetts@gsa.gov>
2021-08-20 10:55:45 -0500

    remove project security module vpc variable

--- formio-project-security/variables.tf
+++ formio-project-security/variables.tf
@@ -16,14 +16,3 @@ variable "region" {
   type        = string
   description = "AWS region"
 }
-
-variable "vpc_id" {
-  type        = string
-  description = "Used the vpc id"
-}
-
-variable "documentdb_allowed_cidr_blocks" {
-  type        = list(string)
-  default     = []
-  description = "List of CIDR blocks to be allowed to connect to the DocumentDB cluster"
-}
