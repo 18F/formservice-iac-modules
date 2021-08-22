ee10c69cd9defe71401463845ec5c4e58e15f86e
Michael Willetts <michael.willetts@gsa.gov>
2021-08-22 12:04:32 -0500

    updates

--- formio-security/main.tf
+++ formio-security/main.tf
@@ -20,30 +20,6 @@ terraform {
 ###################################
 
 
-/* resource "aws_security_group" "documentdb_sg" {
-  name        = "${var.name_prefix}-document-db-sg"
-  description = "Allow Mongo Connections"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = var.documentdb_allowed_cidr_blocks
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-document-db-sg"
-  }
-} */
-
 resource "aws_kms_key" "s3_bucket_key" {
   description               = "${var.name_prefix}-s3-bucket-key"
   key_usage                 = "ENCRYPT_DECRYPT"
