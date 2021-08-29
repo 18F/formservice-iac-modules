8323e7f7df06228fa281d030d84936e613b6734f
Michael Willetts <michael.willetts@gsa.gov>
2021-08-29 06:09:17 -0500

    update vpc endpoints

--- vpc-endpoints/main.tf
+++ vpc-endpoints/main.tf
@@ -29,12 +29,6 @@ module "vpc_endpoints" {
       service             = "s3"
       tags                = { Name = "${var.name_prefix}-s3-vpc-endpoint" }
     },
-    #dynamodb = {
-      # gateway endpoint
-    #  service         = "dynamodb"
-    #  route_table_ids = [ "rtb-05f5b51d08a2d4b9c", "rtb-030b6d714908a02d9" ]
-    #  tags            = { Name = "${var.name_prefix}-dynamodb-vpc-endpoint" }
-    #},
     sns = {
       service             = "sns"
       subnet_ids          = var.private_subnets
@@ -70,21 +64,109 @@ module "vpc_endpoints" {
       service             = "ecr.api"
       private_dns_enabled = true
       subnet_ids          = var.private_subnets
-      #policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
       tags                = { Name = "${var.name_prefix}-ecr-api-vpc-endpoint" }
     },
     ecr_dkr = {
       service             = "ecr.dkr"
       private_dns_enabled = true
       subnet_ids          = var.private_subnets
-      #policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
       tags                = { Name = "${var.name_prefix}-ecr-dkr-vpc-endpoint" }
     },
-    kms = {
-      service             = "kms"
+    ecs = {
+      service             = "ecs"
       private_dns_enabled = true
       subnet_ids          = var.private_subnets
-      tags                = { Name = "${var.name_prefix}-kms-vpc-endpoint" }
+      tags                = { Name = "${var.name_prefix}-ecs-vpc-endpoint" }
+    },
+    ecs_agent = {
+      service             = "ecs-agent"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-ecs-agent-vpc-endpoint" }
+    },
+    ecs_telemetry = {
+      service             = "ecs-telemetry"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-ecs-telemetry-vpc-endpoint" }
+    },
+    elasticbeanstalk = {
+      service             = "elasticbeanstalk"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-elasticbeanstalk-vpc-endpoint" }
+    },
+    elasticbeanstalk_health = {
+      service             = "elasticbeanstalk-health"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-elasticbeanstalk-health-vpc-endpoint" }
+    },
+    elasticLoadbalancing = {
+      service             = "elasticloadbalancing"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-elasticloadbalancing-vpc-endpoint" }
+    },
+    config = {
+      service             = "config"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-config-vpc-endpoint" }
+    },
+    autoscaling = {
+      service             = "autoscaling"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-autoscaling-vpc-endpoint" }
+    },
+    autoscaling_plans = {
+      service             = "autoscaling-plans"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-autoscaling-plans-vpc-endpoint" }
+    },
+    ec2 = {
+      service             = "ec2"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-ec2-vpc-endpoint" }
+    },
+    ec2_messages = {
+      service             = "ec2messages"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-ec2messages-vpc-endpoint" }
+    },
+    ebs = {
+      service             = "ebs"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-ebs-vpc-endpoint" }
+    },
+    cloudtrail = {
+      service             = "cloudtrail"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-cloudtrail-vpc-endpoint" }
+    },
+    application_autoscaling = {
+      service             = "application-autoscaling"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-application-autoscaling-vpc-endpoint" }
+    },
+     access_analyzer = {
+      service             = "access-analyzer"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-access-analyzer-vpc-endpoint" }
+    },
+     email_smtp = {
+      service             = "email-smtp"
+      private_dns_enabled = true
+      subnet_ids          = var.private_subnets
+      tags                = { Name = "${var.name_prefix}-email-smtp-vpc-endpoint" }
     }
   }
 
