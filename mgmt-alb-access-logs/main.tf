33f98e1820e825f6e7b1f1204c5a89b9a2da20d7
Tyler Wellman <tyler.wellman@bixal.com>
2022-01-28 11:16:33 -0500

    Remove grant

--- mgmt-alb-access-logs/main.tf
+++ mgmt-alb-access-logs/main.tf
@@ -3,11 +3,11 @@ data "aws_canonical_user_id" "current_user" {}
 resource "aws_s3_bucket" "alb_access_logs" {
   bucket = "${var.project}-${var.env}-alb-access-logs"
 
-  grant {
-    id          = data.aws_canonical_user_id.current_user.id
-    type        = "CanonicalUser"
-    permissions = ["READ", "WRITE", "READ_ACP", "WRITE_ACP"]
-  }
+  // grant {
+  //   id          = data.aws_canonical_user_id.current_user.id
+  //   type        = "CanonicalUser"
+  //   permissions = ["READ", "WRITE", "READ_ACP", "WRITE_ACP"]
+  // }
 
   lifecycle_rule {
     abort_incomplete_multipart_upload_days = 0
