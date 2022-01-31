20b51b71da93abcdc277783b0deb778e8a45d8e0
Tyler Wellman <tyler.wellman@bixal.com>
2022-01-31 10:32:31 -0500

    Remove all resources in current module

--- mgmt-alb-access-logs/main.tf
+++ mgmt-alb-access-logs/main.tf
@@ -22,15 +22,15 @@
 //     }
 //   }
 // }
-
-resource "aws_s3_bucket_ownership_controls" "disable_s3_acl" {
-  bucket = "aws_s3_bucket.alb_access_logs.bucket"
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
-
+// 
+// resource "aws_s3_bucket_ownership_controls" "disable_s3_acl" {
+//   bucket = "aws_s3_bucket.alb_access_logs.bucket"
+//
+//   rule {
+//     object_ownership = "BucketOwnerEnforced"
+//   }
+// }
+//
 // resource "aws_s3_bucket_public_access_block" "alb_access_logs" {
 //   bucket = aws_s3_bucket.alb_access_logs.id
 //
