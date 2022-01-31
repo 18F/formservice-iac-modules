74c9ec046a62dfae808d448922b00e9954ebe34a
Tyler Wellman <tyler.wellman@bixal.com>
2022-01-31 16:04:13 -0500

    Update aws_s3_bucket_public_access_block

--- alb-access-logs/main.tf
+++ alb-access-logs/main.tf
@@ -1,27 +1,27 @@
-resource "aws_s3_bucket" "alb_access_logs" {
-  bucket = "${var.project}-${var.env}-alb-access-logs"
-
-  lifecycle_rule {
-    abort_incomplete_multipart_upload_days = 0
-    enabled                                = true
-    id                                     = "${var.project}-90-day-delete"
-
-    expiration {
-      days                         = 90
-      expired_object_delete_marker = false
-    }
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      bucket_key_enabled = false
-
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+// resource "aws_s3_bucket" "alb_access_logs" {
+//   bucket = "${var.project}-${var.env}-alb-access-logs"
+//
+//   lifecycle_rule {
+//     abort_incomplete_multipart_upload_days = 0
+//     enabled                                = true
+//     id                                     = "${var.project}-90-day-delete"
+//
+//     expiration {
+//       days                         = 90
+//       expired_object_delete_marker = false
+//     }
+//   }
+//
+//   server_side_encryption_configuration {
+//     rule {
+//       bucket_key_enabled = false
+//
+//       apply_server_side_encryption_by_default {
+//         sse_algorithm = "AES256"
+//       }
+//     }
+//   }
+// }
 //
 // resource "aws_s3_bucket_ownership_controls" "disable_s3_acl" {
 //   bucket = "aws_s3_bucket.alb_access_logs.bucket"
@@ -34,8 +34,10 @@ resource "aws_s3_bucket" "alb_access_logs" {
 resource "aws_s3_bucket_public_access_block" "alb_access_logs" {
   bucket = aws_s3_bucket.alb_access_logs.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 //
 // resource "aws_s3_bucket_policy" "alb_access_logs" {
