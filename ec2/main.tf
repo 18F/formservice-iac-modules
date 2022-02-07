9d677a785133f4984034d1f004a36aaf960f2de1
Tyler Wellman <tyler.wellman@bixal.com>
2022-02-07 10:44:05 -0500

    Add terraform block, encrypt ebs by default

---
+++ ec2/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  # Live modules pin exact Terraform version; generic modules let consumers pin the version.
+  # The latest version of Terragrunt (v0.25.1 and above) recommends Terraform 0.13.3 or above.
+  required_version = ">= 0.13.3"
+
+  # Live modules pin exact provider version; generic modules let consumers pin the version.
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.7.0"
+    }
+  }
+}
+
+resource "aws_ebs_encryption_by_default" "enabled" {
+  enabled = true
+}
