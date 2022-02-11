510d3b64d11a4b53505e801ef9ce7f1b8c3ce37d
Tyler Wellman <tyler.wellman@bixal.com>
2022-02-11 16:03:33 -0500

    Start new maintenance window module

---
+++ maintenance-window/variables.tf
@@ -0,0 +1,19 @@
+variable "maintenance_window_cutoff" {
+  type        = string
+  description = "The number of hours before the end of the Maintenance Window that Systems Manager stops scheduling new tasks for execution."
+}
+
+variable "maintenance_window_duration" {
+  type        = string
+  description = "The duration of the Maintenance Window in hours."
+}
+
+variable "maintenance_window_name" {
+  type        = string
+  description = "The name of the maintenance window."
+}
+
+variable "maintenance_window_schedule" {
+  type        = string
+  description = "The schedule of the Maintenance Window in the form of a cron or rate expression."
+}
