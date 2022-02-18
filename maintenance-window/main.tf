c96adc8ed9140f30b3f169c88e65667928789909
Tyler Wellman <tyler.wellman@bixal.com>
2022-02-18 10:35:03 -0500

    Update variable name

--- maintenance-window/main.tf
+++ maintenance-window/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_maintenance_window" "this" {
-  name     = var.maintenance_window_name
-  schedule = var.maintenance_window_schedule
-  duration = var.maintenance_window_duration
-  cutoff   = var.maintenance_window_cutoff
+  name     = var.name
+  schedule = var.schedule
+  duration = var.duration
+  cutoff   = var.cutoff
 }
