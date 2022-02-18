6137be486ba9b78a57888b3b3900c748a09f4571
Tyler Wellman <tyler.wellman@bixal.com>
2022-02-18 14:45:45 -0500

    Reorder variables

--- maintenance-window-task/variables.tf
+++ maintenance-window-task/variables.tf
@@ -1,3 +1,13 @@
+variable "cloudwatch_output_enabled" {
+  type        = bool
+  description = "Enables Systems Manager to send command output to CloudWatch Logs."
+}
+
+variable "commands" {
+  type        = list
+  description = "An array of commands to run."
+}
+
 variable "max_errors" {
   type        = number
   description = "The maximum number of errors allowed before this task stops being scheduled."
@@ -6,6 +16,16 @@ variable "max_errors" {
 variable "priority" {
   type        = number
   description = "The priority of the task in the Maintenance Window, the lower the number the higher the priority. Tasks in a Maintenance Window are scheduled in priority order with tasks that have the same priority scheduled in parallel."
+}
+
+variable "target_ids" {
+  type        = list
+  description = "The ids of the targets (either instances or window target ids). Example values: i-0ce6656f9f896e68b, ea9a473f-ae6e-4dbe-854a-4cf4b6da0e0e."
+}
+
+variable "target_type" {
+  type        = string
+  description = "The target type (either instances or window target ids).Valid values: InstanceIds, WindowTargetIds."
 }
 
 variable "task_arn" {
@@ -18,32 +38,12 @@ variable "task_type" {
   description = "The type of task being registered. Valid values: AUTOMATION, LAMBDA, RUN_COMMAND or STEP_FUNCTIONS."
 }
 
-variable "window_id" {
-  type        = string
-  description = "The Id of the maintenance window to register the task with."
-}
-
-variable "target_type" {
-  type        = string
-  description = "The target type (either instances or window target ids).Valid values: InstanceIds, WindowTargetIds."
-}
-
-variable "target_ids" {
-  type        = list
-  description = "The ids of the targets (either instances or window target ids). Example values: i-0ce6656f9f896e68b, ea9a473f-ae6e-4dbe-854a-4cf4b6da0e0e."
-}
-
 variable "timeout_seconds" {
   type        = number
   description = "If this time is reached and the command has not already started executing, it doesn't run."
 }
 
-variable "cloudwatch_output_enabled" {
-  type        = bool
-  description = "Enables Systems Manager to send command output to CloudWatch Logs."
-}
-
-variable "commands" {
-  type        = list
-  description = "An array of commands to run."
+variable "window_id" {
+  type        = string
+  description = "The Id of the maintenance window to register the task with."
 }
