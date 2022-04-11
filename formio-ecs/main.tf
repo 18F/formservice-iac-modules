94e820eb9ec24a0d98fff5d0fbe841b516621b1c
Michael Willetts <michael.willetts@gsa.gov>
2022-04-11 11:14:19 -0500

    add formio ecs cluster module

--- formio-ecs/main.tf
+++ formio-ecs/main.tf
@@ -17,14 +17,15 @@ terraform {
 ####################################
 resource "aws_cloudwatch_log_group" "ecs_logs" {
   name = "${var.name_prefix}-cluster-logs"
+  retention_in_days = 180
 }
 
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "white-hart"
+  name = "${var.name_prefix}-ecs-cluster"
 
   setting {
     name  = "containerInsights"
-    value = "enabled"
+    value = var.enable_container_insights
   }
   configuration {
     execute_command_configuration {
@@ -43,92 +44,6 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   }
 }
 
-resource "aws_ecs_task_definition" "service" {
-  family                = "service"
-  container_definitions = file("task-definitions/service.json")
-
-  volume {
-    name = "service-storage"
-
-    efs_volume_configuration {
-      file_system_id          = aws_efs_file_system.fs.id
-      root_directory          = "/opt/data"
-      transit_encryption      = "ENABLED"
-      transit_encryption_port = 2999
-      authorization_config {
-        access_point_id = aws_efs_access_point.test.id
-        iam             = "ENABLED"
-      }
-    }
-  }
-}
-
-
-resource "aws_ecs_task_definition" "test" {
-  family                   = "test"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
-  container_definitions    = <<TASK_DEFINITION
-[
-  {
-    "name": "iis",
-    "image": "mcr.microsoft.com/windows/servercore/iis",
-    "cpu": 1024,
-    "memory": 2048,
-    "essential": true
-  }
-]
-TASK_DEFINITION
-
-  runtime_platform {
-    operating_system_family = "WINDOWS_SERVER_2019_CORE"
-    cpu_architecture        = "X86_64"
-  }
-}
-
-resource "aws_ecs_task_definition" "service" {
-  family = "service"
-  container_definitions = jsonencode([
-    {
-      name      = "first"
-      image     = "service-first"
-      cpu       = 10
-      memory    = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    },
-    {
-      name      = "second"
-      image     = "service-second"
-      cpu       = 10
-      memory    = 256
-      essential = true
-      portMappings = [
-        {
-          containerPort = 443
-          hostPort      = 443
-        }
-      ]
-    }
-  ])
-
-  volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
-}
 
 
   
--- formio-ecs/outputs.tf
+++ formio-ecs/outputs.tf
@@ -1,19 +1,4 @@
-output "faas_formio_ecs_alb_id" {
-  description = "ALB id for Formio ECS instance"
-  value       = aws_lb.formio_lb.id
-}
-
-output "faas_formio_ecs_alb_dns_name" {
-  description = "ALB DNS name for Formio ECS instance"
-  value       = aws_lb.formio_lb.dns_name
-}
-
-output "faas_formio_ecs_alb_tg_main" {
-  description = "ALB Target Group id for Formio ECS main instance"
-  value       = aws_lb_target_group.main.id
-}
-
-output "faas_formio_ecs_alb_tg_pdf" {
-  description = "ALB Target Group id for Formio ECS pdf server instance"
-  value       = length(aws_lb_target_group.pdf_server[0].id) > 0 ? aws_lb_target_group.pdf_server[0].id : null
+output "ecs_cluster_id" {
+  description = "ID for the ECS Cluster Instance"
+  value       = aws_ecs_cluster.ecs_cluster.id
 }
