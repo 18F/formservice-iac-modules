a1fe40d4d95dc813eaa5255e972193a03f340db8
Michael Willetts <michael.willetts@gsa.gov>
2022-04-18 14:00:08 -0500

    update ecs cluster execute command

--- formio-ecs/main.tf
+++ formio-ecs/main.tf
@@ -29,10 +29,10 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   }
   configuration {
     execute_command_configuration {
-      logging    = "OVERRIDE"
+      logging    = "DEFAULT"
 
       log_configuration {
-        cloud_watch_encryption_enabled = true
+        cloud_watch_encryption_enabled = false
         cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_logs.name
       }
     }
