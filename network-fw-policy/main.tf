158bfb2fefa238ad7bb8af44ba6257344ca8cf1d
Michael Willetts <michael.willetts@gsa.gov>
2022-04-19 14:19:20 -0500

    add fw policy module and update rule outputs

---
+++ network-fw-policy/main.tf
@@ -0,0 +1,19 @@
+
+
+
+
+
+resource "aws_networkfirewall_firewall_policy" "faas_policy" {
+  name = "${var.name_prefix}-firewall-policy"
+
+  firewall_policy {
+    stateless_default_actions          = ["aws:forward_to_sfe"]
+    stateless_fragment_default_actions = ["aws:forward_to_sfe"]
+    dynamic stateless_rule_group_reference {
+      for_each = var.policy_list
+      content {
+        resource_arn = stateless_rule_group_reference.value[rule_arn]
+      }
+    }
+  }
+}
