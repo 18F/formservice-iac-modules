3e7bfe315280da748e12533d5aa1d978ce8ff94e
Michael Willetts <michael.willetts@gsa.gov>
2022-04-22 14:21:02 -0500

    update network fw module for bug

--- network-fw/main.tf
+++ network-fw/main.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_log_group" "firewall_flow" {
   retention_in_days = var.log_retention_days
 }
 
-resource "aws_cloudwatch_log_group" "firewall_alert" {
+resource "aws_cloudwatch_log_group" "firewall_alerts" {
   name = "${var.name_prefix}-firewall-alert"
   retention_in_days = var.log_retention_days
 }
