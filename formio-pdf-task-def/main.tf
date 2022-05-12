bd7d345bddce01033128675e7ee5df30c54feef1
Michael Willetts <michael.willetts@gsa.gov>
2022-05-12 14:45:08 -0500

    update pdf task logging bug

--- formio-pdf-task-def/main.tf
+++ formio-pdf-task-def/main.tf
@@ -252,7 +252,7 @@ resource "aws_ecs_task_definition" "pdf" {
       "logConfiguration": {
           "logDriver": "awslogs",
           "options": {
-              "awslogs-group": "${aws_cloudwatch_log_group.task_logs_proxy.name}}",
+              "awslogs-group": "${aws_cloudwatch_log_group.task_logs_proxy.name}",
               "awslogs-region": "${var.aws_region}",
               "awslogs-stream-prefix": "${var.log_stream_prefix}-proxy"
           }
