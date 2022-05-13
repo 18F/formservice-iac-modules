5aa015a400a043f480a985c0d4c325e5c3707d13
Michael Willetts <michael.willetts@gsa.gov>
2022-05-13 09:57:24 -0500

    update pdf task to escape quotes in cmd and entrypoint

--- formio-pdf-task-def/main.tf
+++ formio-pdf-task-def/main.tf
@@ -212,8 +212,8 @@ resource "aws_ecs_task_definition" "pdf" {
       "cpu": 512,
       "memory": 2048,
       "essential": true,
-      "command": "[\"node\" \"pdf\"]",
-      "entryPoint": "[\"dumb-init\" \"--\"]",
+      "command": ["node", "pdf"],
+      "entryPoint": ["dumb-init", "--"],
       "mountPoints": [
         {
           "sourceVolume": "${var.pdf_volume_name}",
@@ -241,8 +241,8 @@ resource "aws_ecs_task_definition" "pdf" {
       "cpu": 256,
       "memory": 256,
       "essential": true,
-      "command": "[\"nginx\" \"-g\" \"daemon off;\"]",
-      "entryPoint": "[\"/docker-entrypoint.sh\"]",
+      "command": ["nginx", "-g", "daemon off;"],
+      "entryPoint": ["/docker-entrypoint.sh"],
       "mountPoints": [
         {
           "sourceVolume": "${var.pdf_conf_volume_name}",
