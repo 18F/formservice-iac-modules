543a5ab20b29ef0dc0f8d01a977ad8c26c735bec
Michael Willetts <michael.willetts@gsa.gov>
2022-06-02 15:00:26 -0500

    add vpc variable to efs module

--- efs/variables.tf
+++ efs/variables.tf
@@ -10,4 +10,5 @@ variable "allowed_security_groups" {
      type = list(string)
      default = [""]
 }
+variable "vpc_id" { type = string }
 variable "efs_allowed_subnet_cidrs" { type = list(string) }
